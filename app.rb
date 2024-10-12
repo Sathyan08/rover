7ebf1d396dd7fb6c73c8c3a9318faa110ddf30bc
Sathyan Mathai <sathyan.mathai@gmail.com>
2024-10-12 16:04:53 -0400

    feature: handle invalid inputs for CLI

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ require_all 'app'
 
 instruction_filename = RoverTextFileInterpreter::INSTRUCTION_TEXT_FILES[ARGV[0].to_i]
 
+# Default to the first file if the user inputs an invalid index
+instruction_filename ||= RoverTextFileInterpreter::INSTRUCTION_TEXT_FILES.first
+
 puts "Processing file: #{instruction_filename}"
 puts "---------------------------------"
 
