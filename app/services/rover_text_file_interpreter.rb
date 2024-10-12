8c1294351fecf839ca6d748489d1f09f7e586326
Sathyan Mathai <sathyan.mathai@gmail.com>
2024-10-12 14:55:51 -0400

    feature: can interpreting instruction files

---
+++ app/services/rover_text_file_interpreter.rb
@@ -0,0 +1,42 @@
+class RoverTextFileInterpreter
+  INSTRUCTION_TEXT_FILE_PATHS = ['app/data/rover_instructions1.txt', 'app/data/rover_instructions2.txt']
+
+  attr_reader :file_path
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def process
+    boundry = nil
+    File.open(file_path, 'r').each_with_index do |line, index|
+      if index.zero?
+        boundry = create_boundry(line.strip)
+      else
+        rover_string, instructions = line.strip.split(')').map(&:strip)
+        rover_string.gsub!('(', '')
+        rover = create_rover(rover_string)
+        rover_simulation = RoverSimulation.new(rover, boundry, instructions)
+
+        rover_simulation.process_instructions
+        yield(rover_simulation.results)
+      end
+    end
+  end
+
+  private
+
+  def create_boundry(line)
+    x, y = line.split(' ').map(&:to_i)
+    Boundry.new(x, y)
+  end
+
+  def create_rover(rover_string)
+    rover_chars = rover_string.split(', ')
+    rover_coords = rover_chars[0..1].map(&:to_i)
+
+    location = Location.new(*rover_coords)
+    orientation = Orientation.new(rover_chars[2])
+    Rover.new(location, orientation)
+  end
+end
